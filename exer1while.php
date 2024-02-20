9a38a6897ff163b623b3520525b45428e95cacbf
morceli <109186798+joaotds@users.noreply.github.com>
2024-02-20 11:41:10 -0300

    Create exer1while.php

---
+++ exer1while.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Exercicio 1<br>";
+
+$num = 6;
+
+
+$i = 0;
+while($i <= 10) {
+          $mult = $num * $i;
+          echo  $num . " X " . $i . " = " . $mult;
+          echo "<br>";
+
+          $i++;
+}
+?>
